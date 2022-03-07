0ae3c45a992ee06d93b5563724587f419aebaf85
李明燮 <limingxie@limingxie-pc.home.e-kmall.com>
2022-03-08 00:01:18 +0800

    link list

--- linked-list/link_list.go
+++ linked-list/link_list.go
@@ -11,20 +11,51 @@ type Node struct {
 	Next  *Node
 }
 
+// func (l *LinkList) ReverseLinkedList() {
+// 	if l == nil || l.Head == nil {
+// 		return
+// 	}
+// 	reverseLink := LinkList{}
+// 	node := l.Head
+// 	reverseLink.Head = &Node{Value: node.Value}
+// 	for node.Next != nil {
+// 		newNode := reverseLink.Head
+// 		reverseLink.Head = &Node{Value: node.Next.Value}
+// 		reverseLink.Head.Next = newNode
+// 		node = node.Next
+// 	}
+// 	l.Head = reverseLink.Head
+// }
+
+// func (l *LinkList) ReverseLinkedList() {
+// 	if l == nil || l.Head == nil {
+// 		return
+// 	}
+// 	node := Node{Value: l.Head.Value}
+// 	curNode := l.Head
+// 	for curNode.Next != nil {
+// 		newNode := Node{Value: curNode.Value}
+// 		newNode.Next = &node
+// 		node = newNode
+// 		curNode = curNode.Next
+// 		node.Print()
+// 	}
+// 	l.Head = &node
+// }
+
 func (l *LinkList) ReverseLinkedList() {
 	if l == nil || l.Head == nil {
 		return
 	}
-	reverseLink := LinkList{}
-	node := l.Head
-	reverseLink.Head = &Node{Value: node.Value}
-	for node.Next != nil {
-		newNode := reverseLink.Head
-		reverseLink.Head = &Node{Value: node.Next.Value}
-		reverseLink.Head.Next = newNode
-		node = node.Next
+	node := Node{}
+	curNode := l.Head
+	for curNode != nil {
+		newNode := node
+		node.Value = curNode.Value
+		node.Next = &newNode
+		curNode = curNode.Next
 	}
-	l.Head = reverseLink.Head
+	l.Head = &node
 }
 
 func (node *Node) Print() {
@@ -42,6 +73,7 @@ func MainFuncReverseLinkedList() {
 	linkList.Head.Next = &Node{Value: 2}
 	linkList.Head.Next.Next = &Node{Value: 3}
 	linkList.Head.Next.Next.Next = &Node{Value: 4}
+	linkList.Head.Next.Next.Next.Next = &Node{Value: 5}
 
 	linkList.ReverseLinkedList()
 
