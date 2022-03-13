264c57672aa81a0b38dcd6b1d20d2e5b74a8725d
李明燮 <limingxie@limingxie-pc.home.e-kmall.com>
2022-03-14 00:13:45 +0800

    defer

--- godefer/godefer.go
+++ godefer/godefer.go
@@ -3,6 +3,9 @@ package godefer
 import (
 	"errors"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
 func testPanic(x int) {
@@ -40,11 +43,11 @@ func deferTest1() {
 	}(x) // x 被复制
 
 	x += 10
-	y += 100
+	y += 10
 	println("x =", x, "y =", y)
 	/*
-	   x = 20 y = 120
-	   defer: 10 120
+	   x = 20 y = 30
+	   defer: 10 30
 	*/
 }
 
@@ -64,6 +67,129 @@ func deferTest2(a, b int) (i int, err error) {
 		second defer err <nil>
 		first defer err <nil>
 	*/
+}
+
+func deferTest3() error {
+	res, err := http.Get("http://xxxxxxxxxx")
+	if res != nil {
+		defer res.Body.Close()
+	}
+
+	if err != nil {
+		return err
+	}
+
+	// ..code...
+
+	return nil
+}
+
+func deferTest4() {
+	var run func() = nil
+	defer run()
+	fmt.Println("runs")
+}
+
+/*
+func main() {
+    defer func() {
+        if err := recover(); err != nil {
+            fmt.Println(err)
+        }
+    }()
+    deferTest4()
+}
+runs
+runtime error: invalid memory address or nil pointer dereference
+*/
+
+func deferTest5() (i int) {
+
+	i = 0
+	defer func() {
+		fmt.Println(i)
+	}()
+
+	return 2 // print 2
+}
+
+func deferTest6() (err error) {
+	f, err := os.Open("book.txt")
+	if err != nil {
+		return err
+	}
+
+	if f != nil {
+		defer func() {
+			if ferr := f.Close(); ferr != nil {
+				err = ferr
+			}
+		}()
+	}
+
+	// ..code...
+
+	return nil
+}
+
+func deferTest7() error {
+	f, err := os.Open("book.txt")
+	if err != nil {
+		return err
+	}
+	if f != nil {
+		defer func() {
+			if err := f.Close(); err != nil {
+				fmt.Printf("defer close book.txt err %v\n", err)
+			}
+		}()
+	}
+
+	// ..code...
+
+	f, err = os.Open("another-book.txt")
+	if err != nil {
+		return err
+	}
+	if f != nil {
+		defer func() {
+			if err := f.Close(); err != nil {
+				fmt.Printf("defer close another-book.txt err %v\n", err)
+			}
+		}()
+	}
+
+	return nil //输出结果： defer close book.txt err close ./another-book.txt: file already closed
+}
+
+func deferTest7_() error {
+	f, err := os.Open("book.txt")
+	if err != nil {
+		return err
+	}
+	if f != nil {
+		defer func(f io.Closer) {
+			if err := f.Close(); err != nil {
+				fmt.Printf("defer close book.txt err %v\n", err)
+			}
+		}(f)
+	}
+
+	// ..code...
+
+	f, err = os.Open("another-book.txt")
+	if err != nil {
+		return err
+	}
+	if f != nil {
+		defer func(f io.Closer) {
+			if err := f.Close(); err != nil {
+				fmt.Printf("defer close another-book.txt err %v\n", err)
+			}
+		}(f)
+	}
+
+	return nil
 }
 
 func Maingodefer() {
